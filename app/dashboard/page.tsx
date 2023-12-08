40318b0dddea52311442df8780f3d18d479e9e5e
maximBurinskyi <maxim.burinskyi@gmail.com>
2023-12-08 23:12:41 +0200

    skeleton loader

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,35 @@
 import Dropzone from '@/components/Dropzone';
+import { db } from '@/firebase';
+import { FileType } from '@/typings';
 import { auth } from '@clerk/nextjs';
+import console from 'console';
+import { getDocs, collection } from 'firebase/firestore';
 
-function Dashboard() {
+async function Dashboard() {
   const { userId } = auth();
 
+  const docResults = await getDocs(collection(db, 'users', userId!, 'files'));
+  const skeletonFiles: FileType[] = docResults.docs.map((doc) => ({
+    id: doc.id,
+    filename: doc.data().filename || doc.id,
+    timestamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
+    fullName: doc.data().fullName,
+    downloadURL: doc.data().downloadURL,
+    type: doc.data().type,
+    size: doc.data().size,
+  }));
+
+  console.log(skeletonFiles);
+
   return (
-    <main>
-      (use is {userId})
+    <div className="border-t">
+      {/* (use is {userId}) */}
       <Dropzone />
-    </main>
+      <section className="contaner space-y-5">
+        <h2 className="font-bold">All Files</h2>
+        <div></div>
+      </section>
+    </div>
   );
 }
 
