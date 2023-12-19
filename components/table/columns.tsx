59e9a3e7e221daf1d4a9de8b703686e106aca8a9
maximBurinskyi <maxim.burinskyi@gmail.com>
2023-12-19 23:57:42 +0200

    added more columns

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -2,8 +2,26 @@
 
 import { FileType } from '@/typings';
 import { ColumnDef } from '@tanstack/react-table';
+import prettyBytes from 'pretty-bytes';
 
 export const columns: ColumnDef<FileType>[] = [
+  {
+    accessorKey: 'type',
+    header: 'Type',
+    cell: ({ renderValue, ...props }) => {
+      const type = renderValue() as string;
+      const extension: string = type.split('/')[1];
+      return (
+        <div className="w-10">
+          <FileIcon
+            extension={extension}
+            labelColor={COLOR_EXTENSION_MAP[extension]}
+            {...defaultStyles[extension]}
+          />
+        </div>
+      );
+    },
+  },
   {
     accessorKey: 'filename',
     header: 'Filename',
@@ -11,5 +29,25 @@ export const columns: ColumnDef<FileType>[] = [
   {
     accessorKey: 'timestamp',
     header: 'Date added',
+  },
+  {
+    accessorKey: 'size',
+    header: 'Size',
+    cell: ({ renderValue, ...props }) => {
+      return <span>{prettyBytes(renderValue() as number)}</span>;
+    },
+  },
+  {
+    accessorKey: 'downloadURL',
+    header: 'Link',
+    cell: ({ renderValue, ...props }) => (
+      <a
+        href={renderValue() as string}
+        target="_blank"
+        className="underline text-blue-500 hover:text-blue-600"
+      >
+        Download
+      </a>
+    ),
   },
 ];
