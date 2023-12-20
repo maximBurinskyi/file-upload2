68db9298700cfbb52a3ce40351a18a068c44331c
maximBurinskyi <maxim.burinskyi@gmail.com>
2023-12-20 19:21:03 +0200

    files displayed in dashboard

--- components/table/TableWrapper.tsx
+++ components/table/TableWrapper.tsx
@@ -1,12 +1,14 @@
 import { FileType } from '@/typings';
 import { Button } from '../ui/button';
+import { columns } from './columns';
+import { DataTable } from './Table';
 
 function TableWrapper({ skeletonFiles }: { skeletonFiles: FileType[] }) {
   return (
     <div>
       <Button>Sort by ...</Button>
 
-      {/* <DataTable columns={columns} data={initialFiles} /> */}
+      <DataTable columns={columns} data={skeletonFiles} />
     </div>
   );
 }
---
+++ constant.ts
@@ -0,0 +1,6 @@
+export const COLOR_EXTENSION_MAP: { [key: string]: string } = {
+  pdf: '#160FE',
+  docx: '#2b579a',
+  doc: '#2b579a',
+  pptx: '#b7472a',
+};
