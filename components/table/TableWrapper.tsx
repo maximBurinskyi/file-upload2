875d419d467471e5cc7043441d6e3cf419146e36
maximBurinskyi <maxim.burinskyi@gmail.com>
2023-12-21 22:03:22 +0200

    finished tablewrapper

--- components/table/TableWrapper.tsx
+++ components/table/TableWrapper.tsx
@@ -9,6 +9,7 @@ import { useEffect, useState } from 'react';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import { query, collection, orderBy } from 'firebase/firestore';
 import { db } from '@/firebase';
+import { Skeleton } from '@/components/ui/skeleton';
 
 function TableWrapper({ skeletonFiles }: { skeletonFiles: FileType[] }) {
   const { user } = useUser();
@@ -38,9 +39,43 @@ function TableWrapper({ skeletonFiles }: { skeletonFiles: FileType[] }) {
     setInitialFiles(files);
   }, [docs]);
 
+  if (docs?.docs.length === undefined)
+    return (
+      <div className="flex flex-col">
+        <Button variant={'outline'} className="ml-auto w-36 h-10 mb-5">
+          <Skeleton className="h-5 w-full" />
+          {/* Sort by {sort === 'desc' ? 'Newest' : 'Oldest'} */}
+        </Button>
+
+        <div className="border rounded-lg">
+          <div className="border-b h-12">
+            {skeletonFiles.map((file) => (
+              <div
+                key={file.id}
+                className="flex items-center space-x-4 p-5 w-full"
+              >
+                <Skeleton className="h-12 w-12" />
+                <Skeleton className="h-12 w-full" />
+              </div>
+            ))}
+            {setInitialFiles.length === 0 && (
+              <div className="flex items-center space-x-4 p-5 w-full">
+                <Skeleton className="h-12 w-2-12" />
+                <Skeleton className="h-12 w-full" />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+
   return (
-    <div>
-      <Button onClick={() => setSort(sort === 'desc' ? 'asc' : 'desc')}>
+    <div className="flex flex-col space-y-5 pb-10">
+      <Button
+        className="ml-auto w-fit"
+        variant={'outline'}
+        onClick={() => setSort(sort === 'desc' ? 'asc' : 'desc')}
+      >
         Sort by {sort === 'desc' ? 'Newest' : 'Oldest'}
       </Button>
 
